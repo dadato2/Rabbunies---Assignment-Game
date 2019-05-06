7648f9a6e012ff56686633dad81dc9659525be1e
dadato2 <dadato2@users.noreply.github.com>
2019-05-06 23:45:48 +0300

    now has executable file in build directory

---
+++ setup.py
@@ -0,0 +1,14 @@
+import cx_Freeze, os.path
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+executables = [cx_Freeze.Executable("main.py")]
+
+cx_Freeze.setup(
+    name="Rabbunnies: The element of explode!",
+    options={"build_exe": {"packages":["pygame"],
+                           "include_files":["assets"]}},
+    executables=executables
+    )
