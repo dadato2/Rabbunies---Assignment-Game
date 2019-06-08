29e6ad8ce82e9cb45f897aac95fc98e7d8ba7e13
dadato2 <dadato2@users.noreply.github.com>
2019-06-08 17:32:55 +0300

    some changes and things, idk it was old

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
-executables = [cx_Freeze.Executable("main.py")]
+executables = [cx_Freeze.Executable(script="main.py", icon="icon.ico")]
 
 cx_Freeze.setup(
     name="Rabbunnies: The element of explode!",
